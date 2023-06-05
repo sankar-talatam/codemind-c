1fea371ece2466e76557965f3b369a31a33b4bf9
sankar-talatam <119839082+sankar-talatam@users.noreply.github.com>
2023-06-05 10:19:18 +0530

    #include<stdio.h> int main() { 	int m,n; 	scanf("%d %d",&m,&n); 	if(m%2==0 || n%2==0) 	printf("Player 1"); 	else 	printf("Player 2"); }

--- Predict_the_winner_in_Coin_Game.c
+++ Predict_the_winner_in_Coin_Game.c
@@ -4,11 +4,7 @@ int main()
 	int m,n;
 	scanf("%d %d",&m,&n);
 	if(m%2==0 || n%2==0)
-	{
-		printf("Player 1");
-	}
+	printf("Player 1");
 	else
-	{
-		printf("Player 2");
-	}
+	printf("Player 2");
 }
